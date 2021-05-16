a9916c34c200edf19edaee2c223bbb3274f72042
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-16 17:42:00 -0300

    Agregue la migracion para crear la tabla outcomes

---
+++ db/migrations/00008_create_outcomes_table.rb
@@ -0,0 +1,14 @@
+Sequel.migration do
+  up do
+    create_table(:outcomes) do
+      primary_key   :id
+      foreign_key   :choice_id, :choices
+      DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
+      DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
+    end
+  end
+
+  down do
+    drop_table(:outcomes)
+  end
+end
