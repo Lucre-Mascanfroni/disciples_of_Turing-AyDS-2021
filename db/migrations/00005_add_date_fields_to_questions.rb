f499d2e0897a7f7ce30cc4a3f76ab5ce0d5db9eb
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-16 16:53:21 -0300

    add date fields to questions table

---
+++ db/migrations/00005_add_date_fields_to_questions.rb
@@ -0,0 +1,11 @@
+Sequel.migration do
+  up do
+    add_column :questions, :created_at, DateTime, default: Sequel::CURRENT_TIMESTAMP
+    add_column :questions, :updated_at, DateTime, default: Sequel::CURRENT_TIMESTAMP
+  end
+
+  down do
+    drop_column :questions, :created_at
+    drop_column :questions, :updated_at
+  end
+end
