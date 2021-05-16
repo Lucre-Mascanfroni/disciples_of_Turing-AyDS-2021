82550dcc351e4e3aaa51598630185a1ae7fc6d30
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-16 17:03:31 -0300

    Cree la tabla choices

---
+++ db/migrations/00006_create_choices_table.rb
@@ -0,0 +1,14 @@
+Sequel.migration do
+  up do
+    create_table(:choices) do
+      primary_key   :id
+      foreign_key   :question_id, :questions
+      String        :text
+    end
+  end
+
+  down do
+    drop_table(:questions)
+  end
+end
+
