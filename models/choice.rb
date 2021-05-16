13249bf8f2375464dbf7e560a3753b20e74e10f6
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-16 17:53:31 -0300

    Agregue la relacion one_to_many entre choice & outcomes

--- models/choice.rb
+++ models/choice.rb
@@ -1,4 +1,6 @@
 class Choice < Sequel::Model
     # Uses singular form of associated model name
     many_to_one :question
+    # Uses plural form of associated model name
+    one_to_many :outcomes
 end
