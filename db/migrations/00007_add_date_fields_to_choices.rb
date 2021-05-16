a5468c6c0a94af3fdeaf520cf9a0e46b6742ce99
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-16 17:07:44 -0300

    Agregue campos datea la table choices

---
+++ db/migrations/00007_add_date_fields_to_choices.rb
@@ -0,0 +1,12 @@
+Sequel.migration do
+  up do
+    add_column :choices, :created_at, DateTime, default: Sequel::CURRENT_TIMESTAMP
+    add_column :choices, :updated_at, DateTime, default: Sequel::CURRENT_TIMESTAMP
+  end
+
+  down do
+    drop_column :choices, :created_at
+    drop_column :choices, :updated_at
+  end
+end
+
