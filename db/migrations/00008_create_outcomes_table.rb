67dd07161e0f54d11e1bd09a04872c4ca024c18d
Lucre-Mascanfroni <lucre.mascanfroni@gmail.com>
2021-05-17 15:43:39 -0300

    Agregué la clave foránea career_id a la tabla outcomes

--- db/migrations/00008_create_outcomes_table.rb
+++ db/migrations/00008_create_outcomes_table.rb
@@ -3,6 +3,7 @@ Sequel.migration do
     create_table(:outcomes) do
       primary_key   :id
       foreign_key   :choice_id, :choices
+      foreign_key   :career_id, :careers
       DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
       DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
     end
