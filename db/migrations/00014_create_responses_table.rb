e514016473dca34f0bbe01dc230eca19eb6eeb77
tomas98Fer <tomas016fernandez@gmail.com>
2021-05-17 23:59:34 -0300

    Sé creó la tabla responses

---
+++ db/migrations/00014_create_responses_table.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+    up do
+      create_table(:responses) do
+        primary_key   :id
+        foreign_key   :question_id, :questions
+        foreign_key   :choice_id,   :choices
+        foreign_key   :survey_id,   :surveys
+        DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
+        DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
+      end
+    end
+  
+    down do
+      drop_table(:responses)
+    end
+  end
+  
