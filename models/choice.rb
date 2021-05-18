19d2bf93ef44c67a29605920b7f30f248c55dc95
tomas98Fer <tomas016fernandez@gmail.com>
2021-05-18 00:54:59 -0300

    se añadio relación entre las tablas choice y response (many_to_many)

--- models/choice.rb
+++ models/choice.rb
@@ -1,6 +1,8 @@
 class Choice < Sequel::Model
     # Uses singular form of associated model name
-    many_to_one :question
+    many_to_one     :question
     # Uses plural form of associated model name
-    one_to_many :outcomes
+    one_to_many     :outcomes
+    many_to_many    :responses
+
 end
