4ec44e906264e3f579c4b0cc99f715f3109f87b5
Lucre-Mascanfroni <lucre.mascanfroni@gmail.com>
2021-05-20 20:55:01 -0300

    Agregué un test de unidad para verificar que el atributo name sea válido

---
+++ tests/models/surveys_test.rb
@@ -0,0 +1,13 @@
+require File.expand_path '../../test_helper.rb', __FILE__
+
+class SurveyTest < MiniTest::Unit::TestCase
+  MiniTest::Unit::TestCase
+  def test_survey_must_has_username
+    # Arrange
+    survey = Survey.new
+    # Act
+    survey.name = ''
+    # Assert
+    assert_equal survey.valid?, false
+  end
+end
