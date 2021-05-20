af04a1a7e30f1afdeb6b85f4e0f387d4cc3cc7c9
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-20 18:32:55 -0300

    Test para careers

---
+++ tests/models/careers_test.rb
@@ -0,0 +1,17 @@
+require File.expand_path '../../test_helper.rb', __FILE__
+
+class CareerTest < MiniTest::Unit::TestCase
+   MiniTest::Unit::TestCase
+
+   def test_career_has_many_surveys
+        #Arrange
+        
+        career = Career.create(name: 'Licenciatura en Analisis de Capitulos de Teletubbies')
+        #Act
+        Survey.create(name: 'U1', career_id: career.id)
+        Survey.create(name: 'U2', career_id: career.id)
+        Survey.create(name: 'U3', career_id: career.id)
+        #Assert
+        assert_equal career.surveys.count, 3
+   end
+end
