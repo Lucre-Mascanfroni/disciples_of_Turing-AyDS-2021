529fc252392581e0e4ee198b10285f3c8d6e10b3
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-20 20:10:53 -0300

    Agregue un test a questions_test.rb para verificar que efectivamente el atributo number de questions debe ser unico

--- tests/models/questions_test.rb
+++ tests/models/questions_test.rb
@@ -41,10 +41,57 @@ class QuestionTest < MiniTest::Unit::TestCase
    
    def test_question_must_have_all_attributes_5
      #Arrange
-     question = Question.new(name: 'Sky Question', description: 'Is the sky blue?', number: 1, type: 'YES/NO')
+     question = Question.new(name: 'Sky question', description: 'Is the sky blue?', number: 1, type: 'YES/NO')
      #Act
      
      #Assert
      assert_equal question.valid?, true
    end
+   
+   def test_question_number_must_be_unique
+      #Creamos la question en la base de datos
+      Question.create(name: 'Sky question', description: 'Is the sky blue?', number: 1, type: 'YES/NO')
+      assert_equal Question.new(name: 'Question for teacher', description: 'Can i go to the bathroom?', number: 1, type: 'YES/NO').valid?, false
+   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
