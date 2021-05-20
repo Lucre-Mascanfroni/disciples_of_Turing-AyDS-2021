da5c53125b2cd3044aa82c1c4504a682699c450b
Lucre-Mascanfroni <lucre.mascanfroni@gmail.com>
2021-05-20 20:52:54 -0300

    Agregué el puglin y el método validate al modelo survey

--- models/survey.rb
+++ models/survey.rb
@@ -1,4 +1,11 @@
+
 class Survey < Sequel::Model
+	plugin :validation_helpers
     one_to_many    :responses
     many_to_one    :career
+    def validate 
+    	super
+    	validates_presence :name
+    end
 end
+
