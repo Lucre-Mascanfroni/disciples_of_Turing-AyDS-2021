5b095bbc08054f2978039940fcde3699399c445e
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-21 11:17:40 -0300

    Agregue una migracion para remover la tabla posts

---
+++ db/migrations/00013_remove_posts_table.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+    drop_table(:posts)
+  end
+  
+  down do
+    create_table(:posts) do
+      primary_key   :id
+      String        :description
+    end
+  end
+end
+
