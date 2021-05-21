b44daeea1e4a38e35cf907aa1c82b9d24a7d1bc2
Lucre-Mascanfroni <lucre.mascanfroni@gmail.com>
2021-05-20 22:44:36 -0300

    Agregué un test de unidad para verificar que el atributo name no sea nil

--- tests/models/surveys_test.rb
+++ tests/models/surveys_test.rb
@@ -2,12 +2,15 @@ require File.expand_path '../../test_helper.rb', __FILE__
 
 class SurveyTest < MiniTest::Unit::TestCase
   MiniTest::Unit::TestCase
-  def test_survey_must_has_username
-    # Arrange
+  def test_survey_name_must_not_be_empty
     survey = Survey.new
-    # Act
     survey.name = ''
-    # Assert
+    assert_equal survey.valid?, false
+  end
+
+  def test_survey_name_must_not_be_nil
+    survey = Survey.new
+    survey.name = nil
     assert_equal survey.valid?, false
   end
 end
