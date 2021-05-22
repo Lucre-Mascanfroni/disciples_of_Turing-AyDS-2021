22b9e2f7f32959e099c5827332d1acdbd59740d0
tomas98Fer <tomas016fernandez@gmail.com>
2021-05-22 11:21:48 -0300

    se añadió test choices_has_many_outcomes

--- tests/models/choices_test.rb
+++ tests/models/choices_test.rb
@@ -10,4 +10,15 @@ class ChoiceTest < MiniTest::Unit::TestCase
     # Assert
     assert_equal choice.valid?, false
   end
+
+  def test_choice_has_many_outcomes
+    question = Question.create(name:"Pregunta 1",description:"¿Cuáles son tus gustos musicales?",number:1,type:"preferencias")
+    choice = Choice.create(text:'estoy de acuerdo')
+
+    Outcome.create(choice_id: choice.id)
+    Outcome.create(choice_id: choice.id)
+    Outcome.create(choice_id: choice.id)
+    assert_equal choice.outcomes.count, 3
+  end
+  
 end
