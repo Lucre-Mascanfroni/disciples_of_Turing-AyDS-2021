5288e7fa4ca10ed8d86dcf63e3df4a0e4fbf6aaa
Lucre-Mascanfroni <lucre.mascanfroni@gmail.com>
2021-05-22 11:49:43 -0300

    Agregué el método clean_database en cada uno de los test del archivo surveys_test.rb

--- tests/models/surveys_test.rb
+++ tests/models/surveys_test.rb
@@ -3,18 +3,21 @@ require File.expand_path '../../test_helper.rb', __FILE__
 class SurveyTest < MiniTest::Unit::TestCase
   MiniTest::Unit::TestCase
   def test_survey_name_must_not_be_empty
+    clean_database
     survey = Survey.new
     survey.name = ''
     assert_equal survey.valid?, false
   end
 
   def test_survey_name_must_not_be_nil
+    clean_database
     survey = Survey.new
     survey.name = nil
     assert_equal survey.valid?, false
   end
 
   def test_survey_has_many_responses
+    clean_database
     survey = Survey.create(name: 'survey 1')
     Response.create(survey_id: survey.id)
     Response.create(survey_id: survey.id)
@@ -24,6 +27,7 @@ class SurveyTest < MiniTest::Unit::TestCase
   end
 
   def test_survey_has_one_career
+    clean_database
     survey = Survey.create(name: 'survey 1')
     c = Career.create(name: 'Prof. en Ciencias de la Computación')
     survey.career_id = c.id
