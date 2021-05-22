4bd8ac56ee2405f14c02f206ed1518ed0afb6397
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-22 10:47:57 -0300

    Quite comentarios innecesarios & en el primer test limpio la base de datos antes de ejecutar el test para que no se creen conflictos con datos dejados de otros tests

--- tests/models/questions_test.rb
+++ tests/models/questions_test.rb
@@ -4,47 +4,41 @@ class QuestionTest < MiniTest::Unit::TestCase
    MiniTest::Unit::TestCase
 
    def test_question_must_have_all_attributes_1
-     #Arrange
+     #Eliminamos los datos que esten en la base de datos
+     Outcome.dataset.destroy
+     Response.dataset.destroy
+     Survey.dataset.destroy
+     Career.dataset.destroy
+     Choice.dataset.destroy
+     Question.dataset.destroy
+     #Fin de la eliminacion
+   
      question = Question.new(name: 'Sky Question')
-     #Act
-     
-     #Assert
+
      assert_equal question.valid?, false
    end
    
    def test_question_must_have_all_attributes_2
-     #Arrange
      question = Question.new(description: 'Is the sky blue?')
-     #Act
      
-     #Assert
      assert_equal question.valid?, false
    end
    
    def test_question_must_have_all_attributes_3
-     #Arrange
      question = Question.new(number: 1);
-     #Act
      
-     #Assert
      assert_equal question.valid?, false
    end
    
    def test_question_must_have_all_attributes_4
-     #Arrange
      question = Question.new(type: 'YES/NO')
-     #Act
      
-     #Assert
      assert_equal question.valid?, false
    end
    
    def test_question_must_have_all_attributes_5
-     #Arrange
      question = Question.new(name: 'Sky question', description: 'Is the sky blue?', number: 1, type: 'YES/NO')
-     #Act
      
-     #Assert
      assert_equal question.valid?, true
    end
    
@@ -70,7 +64,6 @@ class QuestionTest < MiniTest::Unit::TestCase
    end
    
    def test_question_number_must_be_unique
-      #Creamos la question en la base de datos
       Question.create(name: 'Sky question', description: 'Is the sky blue?', number: 3, type: 'YES/NO')
       assert_equal Question.new(name: 'Question for teacher', description: 'Can i go to the bathroom?', number: 3, type: 'YES/NO').valid?, false
    end
