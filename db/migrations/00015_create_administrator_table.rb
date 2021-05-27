828337b5a94059abdb16147441e6e28cb0172fa0
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-27 18:56:50 -0300

    Agregue una tabla nueva a la base de datos 'Administrator'

---
+++ db/migrations/00015_create_administrator_table.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+    up do
+      create_table(:administrators) do
+        primary_key   :id
+        String        :username
+        String        :password
+        DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
+        DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
+      end
+    end
+  
+    down do
+      drop_table(:administrators)
+    end
+  end
---
+++ models/administrator.rb
@@ -0,0 +1,2 @@
+class Administrator < Sequel::Model
+end
