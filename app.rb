ab3d99503c8b71a8ba68972b32980bd0bfd563c0
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-28 10:51:51 -0300

    Agregue comentarios al app & ademas elimine algunos metodos

--- app.rb
+++ app.rb
@@ -1,33 +1,21 @@
 require './models/init.rb'
 
 class App < Sinatra::Base
+  #GET method of root
   get '/' do
     erb :landing
   end
+  #End of GET method of root
 
-  post '/' do
-    
+  #GET and POST methods of careers
+  get '/careers' do
+    @careers = Career.all
+    erb :careers_index
   end
 
-  get "/hello/:name" do
-   @name = params[:name]
-   erb :hello_template
-  end
-
-  post "/posts" do
-    request.body.rewind  # in case someone already read it
-    data = JSON.parse request.body.read
-    post = Post.new(description: data['desc'])
-    if post.save
-      [201, { 'Location' => "posts/#{post.id}" }, 'CREATED']
-    else
-      [500, {}, 'Internal Server Error']
-    end
-  end
-
-  get '/posts' do
-    p = Post.where(id: 1).last
-    p.description
+  get '/careers/:id' do
+    @career = Career.where(id: params['id']).last
+    erb :careers_id_index
   end
   
   post "/careers" do
@@ -37,18 +25,25 @@ class App < Sinatra::Base
     if action == 'create'
        career = Career.new(name: career_name)
        if career.save
-        redirect '/careers'
+        redirect '/careers' #if the career was saved successfully, we redirect to '/careers'
        else
         [500, {}, 'Internal Server Error']
        end
     else
        career = Career.where(name: career_name).last
+       #maybe the career does not exist in the system, so I ask if it is not nil
        if !career.nil? && career.destroy
-         redirect '/careers'
+         redirect '/careers' #if the career was deleted successfully, we redirect to '/careers'
        else
          [500, {}, 'Internal Server Error']
        end
     end
+  end
+  #End of GET and POST methods of careers
+
+  #GET and POST methods of surveys
+  get '/surveys' do
+    Survey.all.map{ |survey| survey.name }
   end
 
   post "/surveys" do
@@ -61,26 +56,15 @@ class App < Sinatra::Base
       [500, {}, 'Internal Server Error']
     end
   end
+  #End of GET and POST methods of careers
 
-  get '/surveys' do
-    Survey.all.map{ |survey| survey.name }
-  end
-  
-  get '/careers' do
-    @careers = Career.all
-    erb :careers_index
-  end
-
-  get '/careers/:id' do
-    @career = Career.where(id: params['id']).last
-    erb :careers_id_index
-  end
-
+  #POST method of responses
   post '/responses' do
     params[:question_id].each do |q_id|
       r = Response.new(choice_id: params[:"#{q_id}"], survey_id: params[:survey_id], question_id: q_id)
       r.save
     end
   end
+  #End of POST method of responses
 end
 
