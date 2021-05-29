e0fe3875f3f00abb41228dd5a201c0828bdfdc47
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-29 15:06:23 -0300

    Agregue un metodo para actualizar un atributo

--- models/career.rb
+++ models/career.rb
@@ -10,4 +10,9 @@ class Career < Sequel::Model
 	def doesHaveFullDescription?
 		full_description && !full_description.empty?
 	end
+
+	def update_attribute(name, value)
+		send(name.to_s + "=", value)
+		save
+	end
 end
