7923cf1a978ebf3a79eca220f5c939455c9b6b4a
gabrielbaseggio <axelbaseggio@gmail.com>
2021-05-30 16:52:31 -0300

    Agregue un metodo en el modelo survey para obtener el id de la carrera resultado de la encuesta

--- models/survey.rb
+++ models/survey.rb
@@ -3,9 +3,25 @@ class Survey < Sequel::Model
 	plugin :validation_helpers
     one_to_many    :responses
     many_to_one    :career
+
     def validate 
     	super
     	validates_presence :name
+    end
+
+    def id_career_result(careers)
+        h = {}
+        careers.each do |career|
+            h[career.id] = 0
+        end
+
+        responses.each do |response|
+            response.choice.outcomes.each do |outcome|
+                h[outcome.career_id] = h[outcome.career_id] + 1
+            end
+        end
+
+        h.key(h.values.max)
     end
 end
 
