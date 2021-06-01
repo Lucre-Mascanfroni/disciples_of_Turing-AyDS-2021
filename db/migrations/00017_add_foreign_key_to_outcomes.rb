cd91b424815b26b8bfc8e822360194b5698d77b2
gabrielbaseggio <axelbaseggio@gmail.com>
2021-06-01 18:51:00 -0300

    Migracion para agregar la clave foranea career_id a outcomes

---
+++ db/migrations/00017_add_foreign_key_to_outcomes.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+    alter_table(:outcomes) do
+      add_foreign_key :career_id, :careers
+    end
+  end
+
+  down do
+    alter_table(:outcomes) do
+      drop_foreign_key :career_id
+    end
+  end
+end
