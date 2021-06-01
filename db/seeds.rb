a841595218030fe79021add13c1222843a1402b8
Lucre-Mascanfroni <lucre.mascanfroni@gmail.com>
2021-06-01 13:11:14 -0300

    Agregué los atributos 'number' y 'type' en cada question

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,14 @@ quimica = Career.create(name: 'Química')
 sociologia = Career.create(name: 'Sociología')
 veterinaria = Career.create(name: 'Veterinaria')
 
-q1 = Question.new(name: '1. ', description:'Me trasladaría a una zona agrícola-ganadera para ejercer mi profesión.')
+q1 = Question.new(name: '1. ', description:'Me trasladaría a una zona agrícola-ganadera para ejercer mi profesión.', number: 1, type: 'Boolean')
 q1.save
 	ch1_q1 = Choice.new(text:'1. Si', question:q1)
 		Outcome.create(choice: ch1_q1, career: veterinaria)
 		Outcome.create(choice: ch1_q1, career: agronomia)
 	ch2_q1 = Choice.new(text:'2. No', question:q1) 
 
-q2 = Question.new(name:'2. ', description:'Tengo buena memoria y no me cuesta estudiar y retener fórmulas y palabras técnicas.')
+q2 = Question.new(name:'2. ', description:'Tengo buena memoria y no me cuesta estudiar y retener fórmulas y palabras técnicas.', number: 2, type: 'Boolean')
 q2.save
 	ch1_q2 = Choice.new(text:'1. Si', question:q2)
 		Outcome.create(choice: ch1_q2, career: fisica)
@@ -37,71 +37,71 @@ q2.save
 		Outcome.create(choice: ch1_q2, career: computacion)
 	ch2_q2 = Choice.new(text:'2. No', question:q2) 	
 
-q3 = Question.new(name:'3. ', description:'Me gusta escribir. En general mis trabajos son largos y están bien organizados.')
+q3 = Question.new(name:'3. ', description:'Me gusta escribir. En general mis trabajos son largos y están bien organizados.', number: 3, type: 'Boolean')
 q3.save
 	ch1_q3 = Choice.new(text:'1. Si', question:q3)
 		Outcome.create(choice: ch1_q3, career: literatura)
 		Outcome.create(choice: ch1_q3, career: historia)
 	ch2_q3 = Choice.new(text:'2. No', question:q3) 
 
-q4 = Question.new(text:'4. ', description:'Sé quien es Steven Hawking: Conozco y me atraen sus descubrimientos.')
+q4 = Question.new(text:'4. ', description:'Sé quien es Steven Hawking: Conozco y me atraen sus descubrimientos.', number: 4, type: 'Boolean')
 q4.save
 	ch1_q4 = Choice.new(text:'1. Si', question:q4)
 		Outcome.create(choice: ch1_q4, career: fisica)
 	ch2_q4 = Choice.new(text:'2. No', question:q4)
 
-q5 = Question.new(text:'5. ', description: 'Me actualizo respecto de los avances tecnológicos y me intereso por investigar y conocer.')
+q5 = Question.new(text:'5. ', description: 'Me actualizo respecto de los avances tecnológicos y me intereso por investigar y conocer.', number: 5, type: 'Boolean')
 q5.save
 	ch1_q5 = Choice.new(text:'1. Si', question:q5)
 		Outcome.create(choice: ch1_q5, career: computacion)
 	ch2_q5 = Choice.new(text:'2. No', question:q5)
 
-q6 = Question.new(text:'6. ', description: 'Sé quien es Andy Warhol y a qué movimiento artístico pertenece. Me apasiona conocer acerca del arte y sus exponentes.')
+q6 = Question.new(text:'6. ', description: 'Sé quien es Andy Warhol y a qué movimiento artístico pertenece. Me apasiona conocer acerca del arte y sus exponentes.', number: 6, type: 'Boolean')
 q6.save
 	ch1_q6 = Choice.new(text:'1. Si', question:q6)
 		Outcome.create(choice: ch1_q6, career: arte)
 	ch2_q6 = Choice.new(text:'2. No', question:q6)
 
-q7 = Question.new(text:'7. ', description: 'En general me intereso por las dificultades que ha tenido que atravesar la humanidad y cómo lo ha superado.')
+q7 = Question.new(text:'7. ', description: 'En general me intereso por las dificultades que ha tenido que atravesar la humanidad y cómo lo ha superado.', number: 7, type: 'Boolean')
 q7.save
 	ch1_q7 = Choice.new(text: '1. Si', question:q7)
 		Outcome.create(choice: ch1_q7, career: historia)
 	ch2_q7 = Choice.new(text: '2. No', question:q7)
 
-q8 = Question.new(name:'8. ', description:'Me apasiona leer y no me importa si el libro que elijo tiene muchas páginas, para mí es un gran entretenimiento.')
+q8 = Question.new(name:'8. ', description:'Me apasiona leer y no me importa si el libro que elijo tiene muchas páginas, para mí es un gran entretenimiento.', number: 8, type: 'Boolean')
 q8.save
 	ch1_q8 = Choice.new(text:'1. Si', question:q8)
 		Outcome.create(choice: ch1_q8, career: literatura)
 		Outcome.create(choice: ch1_q8, career: historia)
 	ch2_q8 = Choice.new(text:'2. No', question:q8) 
 
-q9 = Question.new(text:'9. ', description: 'Me atrae conocer los procesos y las áreas que hacen funcionar a las empresas.')
+q9 = Question.new(text:'9. ', description: 'Me atrae conocer los procesos y las áreas que hacen funcionar a las empresas.', number: 9, type: 'Boolean')
 q9.save
 	ch1_q9 = Choice.new(text: '1. Si', question:q9)
 		Outcome.create(choice: ch1_q9, career: economia)
 	ch2_q9 = Choice.new(text: '2. No', question:q9)
 
-q10 = Question.new(name:'10. ', description:'Me siento identificado con el pensamiento de algunos filósofos y escritores.')
+q10 = Question.new(name:'10. ', description:'Me siento identificado con el pensamiento de algunos filósofos y escritores.', number: 10, type: 'Boolean')
 q10.save
 	ch1_q10 = Choice.new(text:'1. Si', question:q10)
 		Outcome.create(choice: ch1_q10, career: filosofia)
 		Outcome.create(choice: ch1_q10, career: historia)
 	ch2_q10 = Choice.new(text:'2. No', question:q10) 
 
-q11 = Question.new(text:'11. ', description: 'Me encanta estudiar el cuerpo humano y conocer cómo funciona. Además, no me impresiona la sangre.')
+q11 = Question.new(text:'11. ', description: 'Me encanta estudiar el cuerpo humano y conocer cómo funciona. Además, no me impresiona la sangre.', number: 11, type: 'Boolean')
 q11.save
 	ch1_q11 = Choice.new(text: '1. Si', question:q11)
 		Outcome.create(choice: ch1_q11, career: medicina)
 	ch2_q11 = Choice.new(text: '1. No', question:q11)
 
-q12 = Question.new(name:'12. ', description:'Si pudiera elegir, pasaría mucho tiempo diseñando novedosos objetos o edificios en mi computadora.')
+q12 = Question.new(name:'12. ', description:'Si pudiera elegir, pasaría mucho tiempo diseñando novedosos objetos o edificios en mi computadora.', number: 12, type: 'Boolean')
 q12.save
 	ch1_q12 = Choice.new(text:'1. Si', question:q12)
 		Outcome.create(choice: ch1_q12, career: computacion)
 		Outcome.create(choice: ch1_q12, career: arquitectura)
 	ch2_q12 = Choice.new(text:'2. No', question:q12) 
 
-q13 = Question.new(name:'13. ', description:'Si mi blog fuera temático, trataría acerca de: ')
+q13 = Question.new(name:'13. ', description:'Si mi blog fuera temático, trataría acerca de: ', number: 13, type: 'MChoice')
 q13.save
 	ch1_q13 = Choice.new(text:'1. La importancia de la expresión artística en el desarrollo de la identidad de los pueblos.', question:q13)
 		Outcome.create(choice: ch1_q13, career: sociologia)
@@ -112,7 +112,7 @@ q13.save
 		Outcome.create(choice: ch3_q13, career: geologia)
 	ch4_q13 = Choice.new(text:'4. Ninguna de las opciones.', question:q13)
 
-q14 = Question.new(name:'14. ', description:'Integraría un equipo de trabajo encargado de producir un audiovisual sobre: ')
+q14 = Question.new(name:'14. ', description:'Integraría un equipo de trabajo encargado de producir un audiovisual sobre: ', number: 14, type: 'MChoice')
 q14.save
 	ch1_q14 = Choice.new(text:'1. La práctica de deportes y su influencia positiva en el estado de ánimo de las personas.', question:q14)
 		Outcome.create(choice: ch1_q14, career: educFisica)
@@ -127,7 +127,7 @@ q14.save
 		Outcome.create(choice: ch5_q14, career: economia)
   	ch6_q14 = Choice.new(text:'6. Ninguna de las opciones.', question:q14)
   
-q15 = Question.new(name:'15. ', description:'Sería importante destacarme como: ')
+q15 = Question.new(name:'15. ', description:'Sería importante destacarme como: ', number: 15, type: 'MChoice')
 q15.save
 	ch1_q15 = Choice.new(text:'1. Director de una investigación técnico científica', question:q15)
 		Outcome.create(choice: ch1_q15, career: fisica)
