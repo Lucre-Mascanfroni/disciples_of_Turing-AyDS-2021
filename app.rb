4333e525814ebab6d7357980f779e8b2a478eca5
gabrielbaseggio <axelbaseggio@gmail.com>
2021-06-18 11:08:56 -0300

    Se eliminaron las herramientas del administrador

--- app.rb
+++ app.rb
@@ -13,37 +13,12 @@ class App < Sinatra::Base
   end
   #End of GET method of root
 
-  #GET and POST methods of careers
+  #GET method of careers
   get '/careers' do
     @careers = Career.all
     erb :careers_index
   end
-
-  get '/careers/:id' do
-    @career = Career.find(id: params[:id])
-    erb :career_index
-  end
-  
-  post "/careers" do
-    action    = params[:action]
-    if action == 'create'
-      Career.create(name: params[:name])
-      redirect '/careers'
-    elsif action == 'delete'
-      career = Career.find(id: params[:id])
-
-      if career.nil? || !career.surveys.empty? || !career.outcomes.empty? || !career.destroy then
-        flash[:error] = "Something went wrong trying to delete a career."
-      end
-
-      redirect '/careers'
-    else
-      career = Career.find(id: params[:id])
-      career.update_attribute(params[:attribute], params[:value])
-      redirect "/careers/#{career.id}"
-    end
-  end
-  #End of GET and POST methods of careers
+  #End of GET method of careers
 
   #POST methods of surveys
   post "/surveys" do
@@ -57,7 +32,7 @@ class App < Sinatra::Base
       redirect '/'
     end
   end
-  #End of GET and POST methods of careers
+  #End of POST method of careers
 
   #POST method of responses
   post '/responses' do
@@ -72,40 +47,5 @@ class App < Sinatra::Base
     erb :outcome_index
   end
   #End of POST method of responses
-
-  #GET and POST methods of questions
-  get '/questions' do
-    @questions = Question.all
-    erb :questions_index
-  end
-
-  post '/questions' do
-    action = params[:action]
-    if action == 'create'
-      question = Question.new(name: params[:name], description: params[:description], number: params[:number], type: params[:type])
-      flash[:error] = 'Something went wrong trying to create a question.' if !question.save
-    else
-      question = Question.find(id: params[:id])
-      #if the question does not exist or have associated choices or have associated responses or cannot be deleted
-      if question.nil? || !question.choices.empty? || !question.responses.empty? || !question.destroy
-        flash[:error] = 'Something went wrong trying to delete a question.'
-      end
-    end
-
-    redirect '/questions'
-  end
-  #End of GET and POST method of questions
-  
-  #GET and POST methods of choices
-  post '/choices' do
-    action = params[:action]
-    if action == 'create' && !Question.find(id: params[:question_id]).nil?
-      choice = Choice.new(text: params[:text], question_id: params[:question_id])
-      flash[:error] = 'Something went wrong trying to create the choice.' if !choice.save
-    end
-
-    redirect '/questions'
-  end
-  #End of GET and POST method of choices
 end
 
