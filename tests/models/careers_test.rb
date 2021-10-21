4f65a9588465eae23d49a0a3c79459e11c1626d4
tomas98Fer <tomas016fernandez@gmail.com>
2021-10-21 19:35:20 -0300

    se finalizo con test para nueva funcionalidad de s_for_dates

--- tests/models/careers_test.rb
+++ tests/models/careers_test.rb
@@ -18,5 +18,17 @@ class CareerTest < MiniTest::Unit::TestCase
         assert_equal c.valid?, false
    end
    def test_method_s_for_dates
+     clean_database
+     career = Career.create(name: 'Microbiología')
+     Survey.create(name: 'U1', career_id: career.id, created_at:Time.parse('2021-10-11'))
+     Survey.create(name: 'U2', career_id: career.id, created_at:Time.parse('2021-10-15'))
+     Survey.create(name: 'U3', career_id: career.id, created_at:Time.parse('2021-10-18'))
+     Survey.create(name: 'U4', career_id: career.id, created_at:Time.parse('2021-10-19'))
+     c1 = Career.find(id:career.id)
+     firstDate = '2021-10-11'
+     lastDate = '2021-10-19'
+     result = c1.s_for_dates(firstDate,lastDate)['number']
+     assert_equal result, 4
+
    end
 end
