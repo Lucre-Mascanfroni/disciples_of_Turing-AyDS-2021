a34496fc6ea666e047290bea77826e162023970f
gabrielbaseggio <baseggioaxel@gmail.com>
2021-10-23 19:34:10 -0300

    Se aplico el metodo de Extraccion para evaluar las respuestas

--- models/survey.rb
+++ models/survey.rb
@@ -10,10 +10,14 @@ class Survey < Sequel::Model
     end
 
     def survey_result(careers)
+        evaluate_responses(careers).sort_by { |career, outcomes| outcomes }
+    end
+
+    def evaluate_responses(careers)
         career_weights = {}
         careers.map { |career| career_weights[career] = 0 }
         responses.map { |response| response.choice.outcomes.map { |outcome| career_weights[outcome.career] += 1 } }
-        career_weights.sort_by { |career, outcomes| outcomes }
+        career_weights
     end
 end
 
