4bcea5c07369571fd18ea65ec58d2adecfa674eb
tomas98Fer <tomas016fernandez@gmail.com>
2021-11-21 16:54:41 -0300

    Modify app using MVC patern

--- app.rb
+++ app.rb
@@ -1,45 +1,21 @@
 # frozen_string_literal: true
 
 require './models/init.rb'
+require './controllers/CareerController.rb'
+require './controllers/SurveyController.rb'
+
 
 # Endpoint of the app
 class App < Sinatra::Base
+
+  use CareerController
+  use SurveyController
+
   # GET method of root
   get '/' do
     erb :landing
   end
   # End of GET method of root
-
   
-  # GET methods all surveys given id of the career and two dates.
-  get '/surveys' do
-    @result = {}
-    @result = Career.find(id: params[:careerId]).number_of_surveys_between_two_dates(params[:initial_date], params[:final_date]) if params[:careerId] && params[:initial_date] && params[:final_date]
-    @careers = Career.all
-    erb :number_of_surveys_between_two_dates
-  end
-
-  # POST methods of surveys
-  post '/surveys' do
-    @survey = Survey.new(name: params[:name])
-
-    if @survey.save
-      @questions = Question.all
-      erb :surveys_index
-    else
-      redirect '/'
-    end
-  end
-  # End of POST method of surveys
-
-  # POST method of responses
-  post '/responses' do
-    survey = Survey.find(id: params[:survey_id])
-    params[:question_id].map { |question_id| Response.create(choice_id: params[question_id], survey_id: survey.id, question_id: question_id) }
-    @result = survey.survey_result(Career.all)
-    @career_result = @result[@result.size - 1][0]
-    survey.update(career_id: @career_result.id)
-    erb :outcome_index
-  end
-  # End of POST method of responses
 end
+
